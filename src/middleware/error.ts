3b1639bad8c24b0d926c36658667e3ee59e7b4d1
kalinadukova <kalina984@abv.bg>
2026-01-05 16:54:26 +0200

    refactor: handle multer error

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,14 +1,27 @@
 import type {NextFunction, Request, Response} from "express";
+import multer from "multer";
 
 export function errorMiddleware(err: any, _request: Request, response: Response, _response: NextFunction) {
-    console.log('Error middleware: ', err)
-
     if (err.name === "UnauthorizedError") {
         return response.status(401).json({
             error: "Unauthorized",
             message: err.message,
         });
     }
+
+    if (err instanceof multer.MulterError) {
+        if (err.code === "LIMIT_UNEXPECTED_FILE") {
+            return response.status(400).json({
+                error: "Only one file is allowed"
+            });
+        }
+
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+
+    console.log("Error middleware: ", err);
 
     if (err.statusCode) {
         return response.status(err.statusCode).json({
